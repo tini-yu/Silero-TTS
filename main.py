71dc6351bbae2e2a77c4c90eed117c8d00a451bf
tini-yu <i.novoselova.sas@gmail.com>
2025-03-11 15:34:07 +0300

    init

---
+++ main.py
@@ -0,0 +1,58 @@
+import asyncio
+import os
+import time
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+
+import tts
+
+
+app = FastAPI()
+semaphore = asyncio.Semaphore(30)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Text(BaseModel):
+    text: str
+
+@app.get("/")
+def read_root():
+    return {"Это сервер для ТТС. /audio - чтобы сгенерировать файл"}
+
+
+@app.post("/audio")
+async def generate_audio(input_text: Text):
+
+    #Если запрос находится в очереди больше 300 секунд - возвращает ошибку:
+    start_time = time.time()
+
+    async with semaphore:
+        wait_time = time.time() - start_time
+        if wait_time > 300:
+            start_time = time.time()
+            return {"message": "ОШИБКА: Время ожидания запроса в очереди превысило 300 секунд."}
+        
+        #Если запрос в течении 120 секунд не обработан, возвращает ошибку:
+        try:
+            start = time.time()
+            response = await asyncio.wait_for(asyncio.to_thread(tts.GenerateAudio, input_text), timeout=120)
+            
+            end = time.time()
+            print('It took {} seconds to finish execution.'.format(round(end-start)))
+            return response
+        except asyncio.TimeoutError:
+            return {"message":"ОШИБКА: Время ожидания запроса превысило 120 секунд."}
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8002))  # Default to 8002 if PORT is not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
---
+++ tts.py
@@ -0,0 +1,81 @@
+import io
+import uuid
+from fastapi import Response
+import numpy as np
+from pydantic import BaseModel
+import torch
+import os
+import scipy
+
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'model.pt'
+
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v4_ru.pt',
+                                   local_file)  
+
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+
+
+sample_rate = 24000
+speaker = 'xenia'
+put_accent=True
+put_yo=True
+
+class Text(BaseModel):
+    text: str
+    
+def GenerateAudio(input_text: Text):
+    input_json = input_text.model_dump()
+    input_text = input_json['text']
+
+    try:
+        audio = model.apply_tts(text=input_text,
+                            speaker=speaker,
+                            sample_rate=sample_rate,
+                            put_accent=put_accent,
+                            put_yo=put_yo)
+        
+        audio_buffer = io.BytesIO()
+        # Convert the tensor to a NumPy array
+        if not isinstance(audio, np.ndarray):
+            audio = audio.cpu().numpy()  # Use `.cpu()` if the tensor is on GPU
+
+        # Normalize the audio if it's in float format (e.g., between -1 and 1)
+        if audio.dtype == np.float32 or audio.dtype == np.float64:
+            audio = np.clip(audio, -1.0, 1.0)  # Ensure values are in range [-1, 1]
+            audio = (audio * 32767).astype(np.int16)  # Scale to int16 for WAV format
+        scipy.io.wavfile.write(audio_buffer, rate=sample_rate, data=audio)
+        # audio.write_to_fp(audio_buffer)
+        audio_buffer.seek(0)
+        audio_bytes = audio_buffer.read()
+    except:
+        return {"message":"ОШИБКА: генерация аудио не удалась"}
+
+    # # Тело = текст + разделить + аудио
+    # separator = "---AUDIO---"
+    # body = input_text + separator
+    # body_bytes = body.encode('utf-8') + audio_bytes
+
+    return Response(content = audio_bytes)
+
+    # output_directory = "C:/Users/Joe/Desktop/audio_guide/silero/audiofiles"
+    # os.makedirs(output_directory, exist_ok=True)
+    # unique_filename = f"tts_audio_{uuid.uuid4()}.wav"
+    # file_path = os.path.join(output_directory, unique_filename)
+
+    # # Convert the tensor to a NumPy array
+    # if not isinstance(audio, np.ndarray):
+    #     audio = audio.cpu().numpy()  # Use `.cpu()` if the tensor is on GPU
+
+    # # Normalize the audio if it's in float format (e.g., between -1 and 1)
+    # if audio.dtype == np.float32 or audio.dtype == np.float64:
+    #     audio = np.clip(audio, -1.0, 1.0)  # Ensure values are in range [-1, 1]
+    #     audio = (audio * 32767).astype(np.int16)  # Scale to int16 for WAV format
+        
+    # scipy.io.wavfile.write(file_path, rate=sample_rate,
+    #                     data=audio)
+    
+    # return file_path
